f51019b86a8d84911cd7a707269e26c918670cff
charro <andresgtzmichel@outlook.com>
2025-01-12 10:05:30 -0600

    Add files via upload

---
+++ Python pro/main.py
@@ -0,0 +1,3 @@
+import random
+sobre_mi = [" Mis aficiónes son videojuegos, lectura y deporte", "Tengo la mejor mascota del universo, un pastor belga de 4 años de hermosa personalidad. muy obediente", "En mi proyecto final, cree a mano cerca del 60% de mis sprites", "Me fascina la comida asiatica, mas en concreto la japonesa. ramen y sushi"]
+print(random.choice(sobre_mi))
